76a1a34e3ba31387fe1cda6e28a33c1e57e29e44
Dave Mullally <mullallydavid7@gmail.com>
2023-04-14 18:47:41 +0200

    set up object and its prototype plus expose it globally

--- greetr.js
+++ greetr.js
@@ -1,2 +1,19 @@
 (function (global, $) {
-}(window, jQuery));
+  var Greetr = function (firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
+  };
+
+  Greetr.prototype = {};
+
+  Greetr.init = function (firstName, lastName, language) {
+    var self = this; //so that I dont need to be concerned with what "this" points to later
+    // object constructor
+    self.firstName = firstName || "";
+    self.lastName = lastName || "";
+    self.language = language || "en";
+  };
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
+})(window, jQuery);
