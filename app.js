0be84c8c1cb5786200ffd27da1b333dc3a1eb103
Dave Mullally <mullallydavid7@gmail.com>
2023-04-26 18:06:13 +0200

    try out the library

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('John', 'Doe');
-g.setLang("de").greet(true);
+
+// use our chainable methods
+g.setLang("es").greet(true);
+
+// use our object on the click of the login button
+$('#login').click(function () {
+
+    // create a new 'Greetr' object (let's pretend we know the name from the login)
+    var loginGrtr = G$('John', 'Doe');
+
+    $('#logindiv').hide();
+
+    // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log the welcome as well
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
